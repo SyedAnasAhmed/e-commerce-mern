e51d193cd7d403be7606122beb197308077f2850
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2025-01-07 08:06:48 +0500

    authentication complete

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -15,22 +15,43 @@ export const useUserStore = create((set, get) => ({
     }
 
     try {
-        const res = await axios.post("/auth/signup", { name, email, password });
-        set({ user: res.data, loading: false });
+      const res = await axios.post("/auth/signup", { name, email, password });
+      set({ user: res.data, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.message || "An error occured");
     }
   },
-  login: async ( email, password) => {
+
+  login: async (email, password) => {
     set({ loading: true });
 
     try {
-        const res = await axios.post("/auth/login", { email, password });
-        set({ user: res.data, loading: false });
+      const res = await axios.post("/auth/login", { email, password });
+      // console.log("checking the user state"  , res.data)
+      set({ user: res.data, loading: false });
     } catch (error) {
       set({ loading: false });
       toast.error(error.message || "An error occured");
+    }
+  },
+
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+    try {
+      const response = await axios.get("/auth/profile");
+      set({ user: response.data, checkingAuth: false });
+    } catch (error) {
+      set({ checkingAuth: false, user: null });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axios.post("/auth/logout");
+      set({ user: null });
+    } catch (error) {
+        toast.error(error.response?.data?.message || "An error occured during logout")
     }
   },
 }));
