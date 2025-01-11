479621a489ea9c81d6eb7c606f99b763cab36c9a
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2025-01-11 08:29:33 +0500

    cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,20 @@ import { useEffect } from "react";
 import LoadingSpinner from "./Components/LoadingSpinner";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import { useCartStore } from "./stores/useCartStore";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
+  const {getCartItems} = useCartStore()
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(()=>{
+    getCartItems()
+  },[])
 
   if (checkingAuth) return <LoadingSpinner />;
 
@@ -50,6 +57,7 @@ function App() {
             }
           />
           <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="/cart" element={user ? <CartPage /> : <Navigate to={"/login"}/> } />
         </Routes>
       </div>
       <Toaster />
