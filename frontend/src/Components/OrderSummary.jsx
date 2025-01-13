1e258f6e6d5c5fce29244356dcec40ef7fc36f85
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2025-01-13 22:20:26 +0500

    people also bought

--- frontend/src/Components/OrderSummary.jsx
+++ frontend/src/Components/OrderSummary.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { MoveRight } from "lucide-react";
 
 const OrderSummary = () => {
-  const { total, subtotal, isCouponApplied } = useCartStore();
+  const { total, subtotal, coupon, isCouponApplied } = useCartStore();
   const savings = subtotal - total;
   const formattedSubtotal = subtotal.toFixed(2);
   const formattedTotal = total.toFixed(2);
@@ -60,7 +60,7 @@ const OrderSummary = () => {
           className="flex w-full items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300"
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          onClick={handlePayment}
+        // onClick={handlePayment}
         >
           Proceed to Checkout
         </motion.button>
--- frontend/src/Components/PeopleAlsoBought.jsx
+++ frontend/src/Components/PeopleAlsoBought.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const PeopleAlsoBought = () => {
+
+  const recommendations = []
+
   return (
-    <div>
-      PeopleAlsoBought
+    <div className='mt-8'>
+      <h3 className='text-2xl font-semibold text-emerald-400 '>People Also Bought</h3>
+      <div className='mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+    
+      </div>
     </div>
   )
 }
